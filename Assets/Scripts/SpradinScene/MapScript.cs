9132410f010264832bc485d3d6fd865b3b8c554e
JonSpradlin <129249383+JonSpradlin@users.noreply.github.com>
2023-05-02 20:55:43 -0700

    Waypoint pathing bug

--- Assets/Scripts/SpradinScene/MapScript.cs
+++ Assets/Scripts/SpradinScene/MapScript.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 
 public class MapScript : MonoBehaviour
@@ -13,6 +15,9 @@ public class MapScript : MonoBehaviour
     {
         //Start map hiding waypoints from player.
         tagItems = GameObject.FindGameObjectsWithTag("waypoints");
+        tagItems = sort(tagItems); //sort by numeric in object name
+        
+
         foreach (GameObject tagItem in tagItems)
         {
             tagItem.GetComponent<Renderer>().enabled = false;
@@ -51,5 +56,22 @@ public class MapScript : MonoBehaviour
         return tagItems;
     }
 
+    private GameObject[] sort(GameObject[] tag)
+    {
+        //temporary array of size tag.length
+        GameObject[] temp = new GameObject[tag.Length];
 
+        //holder of number extracted from object name
+        string curVal;
+
+        //reorder array using number as index
+        foreach(GameObject tagItem in tag)
+        {
+            curVal = Regex.Match(tagItem.name, @"\d+").Value;
+            temp[int.Parse(curVal)] = tagItem;
+        }
+
+        return temp;
+    }
 }
+

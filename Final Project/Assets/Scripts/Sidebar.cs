414dd2f8a65815df94f7937c8aef3e7ab231eaa7
JonSpradlin <129249383+JonSpradlin@users.noreply.github.com>
2023-05-06 18:00:40 -0700

    Tower Upgrades in work
    unity throwing method not supported exception but code works as intended. revolves around using getComponent<spriteRenderer>(); will continue work

---
+++ Final Project/Assets/Scripts/Sidebar.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sidebar : MonoBehaviour
+{
+    public string towerName;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(towerName != "")
+        {
+            towerName = "Prefabs/" + towerName;
+         //   Object temp = Resources.Load(towerName);
+          //  SpriteRenderer temp2 = temp.GetComponent<SpriteRenderer>();
+            //Sprite img = temp2.sprite;
+           // Sprite img = Resources.Load(towerName).GetComponent<SpriteRenderer>().sprite;
+            //this.GetComponent<Button>().image.sprite = img;
+        }
+       
+
+
+
+
+
+
+    }
+}

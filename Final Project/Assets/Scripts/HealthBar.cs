fb9941f0a2ca44629c1986aca15489f7720ff91f
JonnyBoy8 <jtha99@uw.edu>
2023-05-14 22:09:39 -0700

    healthbars now stay on bottom of enemies

--- Final Project/Assets/Scripts/HealthBar.cs
+++ Final Project/Assets/Scripts/HealthBar.cs
@@ -22,7 +22,13 @@ public class HealthBar : MonoBehaviour
         // Make the child object look at the target position
         transform.LookAt(targetPosition);
 
-        //
+        Vector3 parentPosition = transform.parent.position;
+
+        // Calculate the offset to move the child object to the bottom
+        float childOffset = transform.localScale.y * 0.5f;
+
+        // Set the child object's position
+        transform.position = parentPosition + new Vector3(0f, -childOffset, 0f);
     }
 
     public void SetMaxHealth(float health)
@@ -36,6 +42,8 @@ public class HealthBar : MonoBehaviour
     public void SetHealth(float health)
     {
         slider.value = health;
+
+        //something wrong with the sun
         fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
